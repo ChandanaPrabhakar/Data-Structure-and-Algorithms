b6df8bd2145751098ceb1e50c02542c711963d7b
ChandanaPrabhakar <154395526+ChandanaPrabhakar@users.noreply.github.com>
2025-01-19 09:40:17 +0530

    Selection Sort

---
+++ Algorithms/SortingAlgorithms/SelectionSort.java
@@ -0,0 +1,34 @@
+package Algorithms.SortingAlgorithms;
+import java.util.*;
+
+class SelectionSort{
+
+    static void selectionSort(int arr[], int n){
+        for(int i=0; i<n-1; i++){
+            int mini_index = i;
+            for(int j=i+1; j<n; j++){
+                if(arr[j] < arr[mini_index]){
+                    mini_index = j;
+                }
+            }
+            int temp = arr[i];
+            arr[i] = arr[mini_index];
+            arr[mini_index] = temp;
+        }
+
+        for(int index=0; index < n; index++){
+            System.out.print(arr[index]+" ");
+        }
+    }
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter the length of array");
+        int n = in.nextInt();
+        int arr[] = new int[n];
+        for(int i=0; i<n;i++){
+            arr[i] = in.nextInt();
+        }
+        selectionSort(arr, n);
+        in.close();
+    }
+}
