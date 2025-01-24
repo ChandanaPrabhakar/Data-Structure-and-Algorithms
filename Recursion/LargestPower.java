aa8b120415a42fc79b74f0081636f3931c9e8d48
ChandanaPrabhakar <154395526+ChandanaPrabhakar@users.noreply.github.com>
2025-01-24 10:37:04 +0530

    Legendre’s formula – Largest power of a prime p in n!

---
+++ Recursion/LargestPower.java
@@ -0,0 +1,20 @@
+package Recursion;
+import java.util.*;
+
+public class LargestPower {
+    public static int largestPowerofP(int num, int p){
+        if(num == 0){
+            return 0;
+        }
+        return num/p + largestPowerofP(num/p, p);
+    }
+    public static void main(String [] args){
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter the integer num: ");
+        int num = scan.nextInt();
+        System.out.println("Enter the prime number p:");
+        int p = scan.nextInt();
+        System.out.println("largest x such that px divides n!"+largestPowerofP(num, p));
+        scan.close();
+    }
+}
