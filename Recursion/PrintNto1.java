c015d1e49c560a0f004dc1a8e56f8617f5e8106a
ChandanaPrabhakar <154395526+ChandanaPrabhakar@users.noreply.github.com>
2025-01-24 13:22:25 +0530

    Print N to 1 without loop

---
+++ Recursion/PrintNto1.java
@@ -0,0 +1,19 @@
+package Recursion;
+import java.util.*;
+
+public class PrintNto1 {
+    public static void PrintNumToOne(int num){
+        if(num > 0){
+            System.out.print(num+" ");
+            PrintNumToOne(num-1);
+        }
+        return;
+    }
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter an integernum: ");
+        int num = scan.nextInt();
+        PrintNumToOne(num);
+        scan.close();
+    }
+}
