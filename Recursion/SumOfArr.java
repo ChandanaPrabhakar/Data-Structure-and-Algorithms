eba3aacf6956ac829de0a55100eb41e1f8d08dcc
ChandanaPrabhakar <154395526+ChandanaPrabhakar@users.noreply.github.com>
2025-01-25 08:22:41 +0530

    Sum of array elements using recursion

---
+++ Recursion/SumOfArr.java
@@ -0,0 +1,24 @@
+package Recursion;
+import java.util.*;
+
+public class SumOfArr {
+    public static int SumOfArrayElements(int arr[], int length){
+        if(length == 1 ){
+            return arr[length-1];
+        }
+        return arr[length-1] + SumOfArrayElements(arr, length-1);
+    }
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter the length of array: ");
+        int length = scan.nextInt();
+        System.out.println("Enter array elements: ");
+        int arr[] = new int[length];
+        for(int i=0; i <length; i++){
+            arr[i] = scan.nextInt();
+        }
+        System.out.println(SumOfArrayElements(arr, length));
+        scan.close();
+    }
+    
+}
