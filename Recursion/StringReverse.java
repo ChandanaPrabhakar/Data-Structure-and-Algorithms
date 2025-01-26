bb00438d92950fac2dfc6e57b9cd09afebd698b1
ChandanaPrabhakar <154395526+ChandanaPrabhakar@users.noreply.github.com>
2025-01-26 19:04:55 +0530

    Print reverse of a string using recursion

---
+++ Recursion/StringReverse.java
@@ -0,0 +1,21 @@
+package Recursion;
+import java.util.*;
+
+public class StringReverse {
+    public static String RecStringReverse(String str, int length){
+        if(length < 1){
+            return "";
+        }
+        if(length == 1){
+            return String.valueOf(str.charAt(length-1));
+        }
+        return str.charAt(length-1)+ RecStringReverse(str, length-1);
+    }
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter the string: ");
+        String str = scan.nextLine();
+        System.out.print(RecStringReverse(str, str.length()));
+        scan.close();
+    }
+}
