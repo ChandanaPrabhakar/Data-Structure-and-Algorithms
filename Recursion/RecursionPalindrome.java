c9e3bc7719c09b79d7fa47a608bc117c5b23af61
ChandanaPrabhakar <154395526+ChandanaPrabhakar@users.noreply.github.com>
2025-01-27 10:09:25 +0530

    Recursive function to check if a string is palindrome

---
+++ Recursion/RecursionPalindrome.java
@@ -0,0 +1,33 @@
+package Recursion;
+import java.util.*;
+
+public class RecursionPalindrome {
+    public static boolean recPalindrome(String str, int start, int end){
+        if(start == end){
+            return true;
+        }
+        if(str.charAt(start) != str.charAt(end)){
+            return false;
+        }
+
+        if(start < end + 1){
+            return recPalindrome(str, start+1, end-1);
+        }
+
+        return true;
+    }
+    public static boolean isPalindrome(String str){
+        int length = str.length();
+        if(length == 0){
+            return true;
+        }
+        return recPalindrome(str, 0, length-1);
+    }
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter string(str): ");
+        String str1 = scan.nextLine();
+        System.out.println(isPalindrome(str1));
+        scan.close();
+    }
+}
