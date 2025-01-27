d693c3516e36b5bd06f4a3b6b4593887b400fd04
ChandanaPrabhakar <154395526+ChandanaPrabhakar@users.noreply.github.com>
2025-01-27 09:08:37 +0530

    Find First n Fibonacci Numbers

---
+++ Recursion/FibonacciNumber.java
@@ -0,0 +1,31 @@
+package Recursion;
+import java.util.*;
+
+public class FibonacciNumber {
+    public static int findfibonaccinumber(int num){
+        if(num == 0){
+            return 0;
+        }else if(num == 1){
+            return 1;
+        }else{
+            return findfibonaccinumber(num - 2) + findfibonaccinumber(num - 1);
+        }
+    }
+    static List<Integer> FindFibonacciNumber(int num) {
+        List<Integer> ans = new ArrayList<>();
+        for (int i = 0; i < num; i++) {
+           ans.add(findfibonaccinumber(i));
+        }
+        return ans;
+     }
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter the number num: ");
+        int num = scan.nextInt();
+        List<Integer> res = FindFibonacciNumber(num);
+        for (int i = 0; i < res.size(); i++) {
+            System.out.print(res.get(i) + " ");
+        }
+        scan.close();
+    }
+}
