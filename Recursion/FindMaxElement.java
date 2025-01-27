31e4fb2379c2157abe16af42b8f9fba79a50d197
ChandanaPrabhakar <154395526+ChandanaPrabhakar@users.noreply.github.com>
2025-01-27 09:32:50 +0530

    Find Maximum Element in Array using Recursion

---
+++ Recursion/FindMaxElement.java
@@ -0,0 +1,23 @@
+package Recursion;
+import java.util.*;
+
+public class FindMaxElement {
+    public static int FindMaxElementOfArray(int arr[], int length){
+        if(length == 1){
+            return arr[length-1];
+        }
+        return Math.max(arr[length-1], FindMaxElementOfArray(arr, length-1));
+    }
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter array length: ");
+        int length = scan.nextInt();
+        System.out.println("Enter array elements: ");
+        int arr[] = new int[length];
+        for(int i = 0; i < length; i++){
+            arr[i] = scan.nextInt();
+        }
+        System.out.println(FindMaxElementOfArray(arr, length));
+        scan.close();
+    }
+}
