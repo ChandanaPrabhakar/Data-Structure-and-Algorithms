1f2edd3c1caf4902ca43900690c4727550acf8c9
ChandanaPrabhakar <154395526+ChandanaPrabhakar@users.noreply.github.com>
2025-01-29 22:45:42 +0530

    Java program to print Inverted Triangle of number of rows

---
+++ Patterns/InvertedTriangleNum.java
@@ -0,0 +1,20 @@
+package Patterns;
+import java.util.*;
+
+public class InvertedTriangleNum {
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter the number of rows: ");
+        int rows = scan.nextInt();
+        for(int i = 0; i < rows; i++){
+            for(int j = 0; j < i; j++){
+                System.out.print(" ");
+            }
+            for(int j = 0; j < rows - i; j++){
+                System.out.print(i+1);
+            }
+            System.out.println();
+        }
+        scan.close();
+    }
+}
