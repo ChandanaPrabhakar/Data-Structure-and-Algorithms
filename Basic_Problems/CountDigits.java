6237c5bbe7bf55420677d2fae05e6960d109433e
ChandanaPrabhakar <154395526+ChandanaPrabhakar@users.noreply.github.com>
2025-03-07 15:59:52 +0530

    added O(1) approach

--- Basic_Problems/CountDigits.java
+++ Basic_Problems/CountDigits.java
@@ -2,6 +2,8 @@ package Basic_Problems;
 import java.util.*;
 
 public class CountDigits {
+
+    //O(log(n))
     public static int countOfDigits(int number){
         if(number == 0){
             return 1;
@@ -13,11 +15,19 @@ public class CountDigits {
         }
         return count;
     }
+
+    // O(1) approach
+
+    public static int countDigits(int number){
+        String numberToString = Integer.toString(number);
+        return numberToString.length();
+    }
     public static void main(String[] args){
         Scanner scan = new Scanner(System.in);
         System.out.println("Enter the number: ");
         int number = scan.nextInt();
-        System.out.println("The count of digits in "+number+" : "+countOfDigits(number));
+        System.out.println("The count of digits in "+number+" (O(log(n))): "+countOfDigits(number));
+        System.out.println("The count of digits in "+number+" (O(1)): "+countDigits(number));
         scan.close();
     }
 }
