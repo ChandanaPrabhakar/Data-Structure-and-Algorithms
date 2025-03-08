ada64c5dfbcaee4086bdd993ce493816fb6fd100
ChandanaPrabhakar <154395526+ChandanaPrabhakar@users.noreply.github.com>
2025-03-08 17:02:28 +0530

    optimized from O(n) to O(sqrt(n))

--- Basic_Problems/PrimeTesting.java
+++ Basic_Problems/PrimeTesting.java
@@ -7,7 +7,7 @@ public class PrimeTesting {
             return false;
         }
 
-        for(int i = 2; i < number; i++){
+        for(int i = 2; i < Math.sqrt(number); i++){
             if(number % i == 0){
                 return false;
             }
