82b22bfad63769b80179a627c47e0be84a5eef5c
ChandanaPrabhakar <154395526+ChandanaPrabhakar@users.noreply.github.com>
2026-02-05 21:19:56 +0530

    Upper Bound (smallest > target)

---
+++ Algorithms/BinarySearchProblems/Foundation/UpperBound.java
@@ -0,0 +1,30 @@
+package Algorithms.BinarySearchProblems.Foundation;
+
+public class UpperBound {
+    public static void main(String[] args) {
+        int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+        int target = 5;
+        int result = binarySearch(arr, target);
+        if (result == -1) {
+            System.out.println("Element not found");
+        } else {
+            System.out.println("Index of upper bound element: " + result);
+        }
+    }
+
+    public static int binarySearch(int[] array, int target) {
+        int left = 0, right = array.length - 1, ans = -1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (array[mid] > target) {
+                ans = mid;
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return ans;
+    }
+}
